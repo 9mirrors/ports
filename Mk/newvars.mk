021a51c746077fe794b1adecade3126b3615724d
Matthew Veety <mveety@gmail.com>
2015-05-15 23:07:02 -0400

    added prelim support for lowercase variable names. UPPERCASE still works fine and will for a while

---
+++ Mk/newvars.mk
@@ -0,0 +1,64 @@
+# AUTOMATICALLY GENERATED FILE -- DO NOT EDIT
+if(! ~ $#portname 0){
+	PORTNAME=$portname
+}
+if(! ~ $#version 0){
+	VERSION=$version
+}
+if(! ~ $#description 0){
+	DESCRIPTION=$description
+}
+if(! ~ $#owner 0){
+	OWNER=$owner
+}
+if(! ~ $#websource 0){
+	WEBSOURCE=$websource
+}
+if(! ~ $#workfile 0){
+	WORKFILE=$workfile
+}
+if(! ~ $#9pserver 0){
+	9PSERVER=$9pserver
+}
+if(! ~ $#9pfile 0){
+	9PFILE=$9pfile
+}
+if(! ~ $#9pdir 0){
+	9PDIR=$9pdir
+}
+if(! ~ $#extractcommand 0){
+	EXTRACTCOMMAND=$extractcommand
+}
+if(! ~ $#hgsource 0){
+	HGSOURCE=$hgsource
+}
+if(! ~ $#buildonly 0){
+	BUILDONLY=$buildonly
+}
+if(! ~ $#deps 0){
+	DEPS=$deps
+}
+if(! ~ $#buildpath 0){
+	BUILDPATH=$buildpath
+}
+if(! ~ $#patches 0){
+	PATCHES=$patches
+}
+if(! ~ $#buildcommand 0){
+	BUILDCOMMAND=$buildcommand
+}
+if(! ~ $#message 0){
+	MESSAGE=$message
+}
+if(! ~ $#installcommand 0){
+	INSTALLCOMMAND=$installcommand
+}
+if(! ~ $#cleancommand 0){
+	CLEANCOMMAND=$cleancommand
+}
+if(! ~ $#uninstall 0){
+	UNINSTALL=$uninstall
+}
+if(! ~ $#uninstallcommand 0){
+	UNINSTALLCOMMAND=$uninstallcommand
+}
