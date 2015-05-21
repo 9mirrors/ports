89ec02f8784bb22da9383fffacba4f161ff19f05
Jens Staal <staal1978@gmail.com>
2015-05-21 11:06:12 +0200

    getopt for APE

---
+++ ape-libs/libgetopt/files/getopt.h
@@ -0,0 +1,13 @@
+#ifndef GETOPT_H
+#define GETOPT_H
+#pragma lib "/$M/lib/ape/libgetopt.a"
+
+#define getopt gnu_getopt
+#define getopt_long gnu_getopt_long
+#include "gnu_getopt.h"
+
+
+
+#endif
+
+
