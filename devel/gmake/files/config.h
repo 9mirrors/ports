0861c1881b714a8808854ea4d7b0cd31703df7fe
Jens Staal <staal1978@gmail.com>
2015-06-10 19:32:42 +0200

    GNU make port to Plan9

---
+++ devel/gmake/files/config.h
@@ -0,0 +1,19 @@
+/* manually generated config.h to build gmake on Plan9 */
+
+#define uintmax_t unsigned long
+
+#define LIBDIR "/$M/lib/ape/" /* or should it be $objtype ? */
+#define INCLUDEDIR "/sys/include/ape"
+
+#define VERSION "4.1"
+#define PACKAGE "gmake"
+#define LOCALEDIR "/sys/lib/locale"
+
+#define SCCS_GET "sccs get"
+#define FILE_TIMESTAMP_HI_RES 0
+
+
+
+
+
+
