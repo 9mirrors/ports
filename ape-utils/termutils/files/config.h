00acda3e6a43c971456efe85f2d483772e0272fe
Jens Staal <staal1978@gmail.com>
2015-06-14 12:03:52 +0200

    termutils for APE

---
+++ ape-utils/termutils/files/config.h
@@ -0,0 +1,28 @@
+/* config.h generated manually  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define as the return type of signal handlers (int or void).  */
+#define RETSIGTYPE void
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the tcgetattr function.  */
+#define HAVE_TCGETATTR 1
+
+/* Define if you have the <sgtty.h> header file.  */
+#undef HAVE_SGTTY_H
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <termios.h> header file.  */
+#define HAVE_TERMIOS_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
