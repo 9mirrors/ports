9e55f1d1cde6be64503064751b8c664bbc4c6e91
Jens Staal <staal1978@gmail.com>
2015-06-14 17:24:29 +0200

    lzo lib and lzop utility

---
+++ archivers/lzop/files/config.h
@@ -0,0 +1,19 @@
+/* manually generated config.h to fix compilation issues */
+
+#include <sys/types.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+
+
+
+
+
+
